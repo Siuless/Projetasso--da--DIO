199a41bdd097d3027f17fb9951759c19f0034a7b
Siuless <luiszandonadicamargo@gmail.com>
2024-01-13 14:57:09 -0300

    meu projeto da DIO

--- projetasso.js
+++ projetasso.js
@@ -1,1 +1,23 @@
-console.log ("ola mundao")
+let nomeH = "Siulzinho"
+
+let nivel;
+let xpHeroi = 8912
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {xpHeroi >= 10001
+    nivel = "Radiante"
+}
+console.log("O Herói de nome" + nomeH +  "está no nível de " + nivel)
